27dc8b840770571ec87023cdf5c1910ce9b65a84
161710144 <161710144@smkassalaambandung.sch.id>
2018-08-06 17:03:17 +0700

    laratrust

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Role;
+use App\User;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call(UsersSeeder::class);
+    }
+}
